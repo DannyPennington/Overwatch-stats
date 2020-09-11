adb087e518b4f79fb5802667adfd3dd403c65149
Daniel Pennington <51288183+DannyPennington@users.noreply.github.com>
2020-09-11 16:14:23 +0100

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -12,7 +12,7 @@ ax.set_xlabel('Arg number', fontsize=14)
 ax.set_ylabel('Value', fontsize=14)
 ax.set_xlim(0, len(x)+1)
 ax.set_ylim(0, arg2+1)
-plt.title(f"Get fucked nerd arg:{arg1} arg2:{arg2}", fontsize="16")
+plt.title(f"What a lovely graph arg:{arg1} arg2:{arg2}", fontsize="16")
 
 plt.savefig(f'public/images/1_{arg1},2_{arg2}.png')
 
